06e16fb16e35a2cdd3d8f7244654b6c981a1cd9f
John Kibirige <kibirigejohn005@gmail.com>
2022-09-16 09:52:44 +0300

    Add form to Books.jsx component

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# bookstore-react
+# Bookstore
+
+This is a simple web app that allows a user to enter a given book and its author after which they are displayed as a list of many books.
+The user is also able to remove a given book from the list
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -9,7 +9,17 @@ const Books = () => {
   const bookElements = books.map((book) => (
     <Book title={book.title} author={book.author} key={book.title} />
   ));
-  return <div className='book-elements'>{bookElements}</div>;
+  return (
+    <div className='books-container'>
+      <div className='book-elements'>{bookElements}</div>
+      <form action='#'>
+        <h3>Add new book</h3>
+        <input type='text' className='bk-title' placeholder='title' />
+        <input type='text' className='author' placeholder='author' />
+        <button className='add-book-btn'>add book</button>
+      </form>
+    </div>
+  );
 };
 
 export default Books;
