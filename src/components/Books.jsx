21e1cce976eeec9147c83df7ee09ed2be40f02a3
John Kibirige <kibirigejohn005@gmail.com>
2022-09-16 09:25:08 +0300

    Create a dummy list of books to be displayed in Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
+import Book from './Book';
 
-const Book = () => {
-  return <div>A list of books</div>;
+const Books = () => {
+  const [books, setBooks] = useState([
+    { title: 'just a title', author: 'Some author' },
+  ]);
+  const bookElements = books.map((book) => (
+    <Book title={book.title} author={book.author} key={book.title} />
+  ));
+  return <div className='book-elements'>{bookElements}</div>;
 };
 
-export default Book;
+export default Books;
