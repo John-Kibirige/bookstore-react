68295b51b239d94330898893c2b6c27f0649a0eb
John Kibirige <kibirigejohn005@gmail.com>
2022-09-16 08:45:09 +0300

    Setup Book and categories routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Book from './components/Book';
+import Categories from './components/Categories';
+import SharedLayout from './components/SharedLayout';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SharedLayout />}>
+            <Route index element={<Book />} />
+            <Route path='categories' element={<Categories />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
