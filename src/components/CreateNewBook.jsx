7299fe1078a2fe4ed88805eb3f55c09b664158e3
John Kibirige <kibirigejohn005@gmail.com>
2022-09-20 18:28:51 +0300

    Fix linter errors

--- src/components/CreateNewBook.jsx
+++ src/components/CreateNewBook.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { v4 as getUniqueId } from 'uuid';
 import { addBookActionCreator } from '../redux/books/book';
