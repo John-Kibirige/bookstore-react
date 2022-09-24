50814f1b3f69cd4a02caea9889696ffd1b470e65
John Kibirige <kibirigejohn005@gmail.com>
2022-09-24 08:01:21 +0300

    Load all the available books on the api when the page is first loaded

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-
 import App from './App';
+import {fetchBooks }from './redux/books/book';
+
+store.dispatch(fetchBooks());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
